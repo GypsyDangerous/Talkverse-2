933a6cd7c874e223f65018949b1bd2433e26f146
David Snyder <davidgraygs4@gmail.com>
2021-03-13 13:04:44 -0500

    implement routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,49 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HomeComponent } from "./home/home.component";
+import { AuthComponent } from "./auth/auth.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { ChannelComponent } from "./channel/channel.component";
 
-const routes: Routes = [];
+import { ChannelGuard } from "./guards/channel.guard";
+import { AuthGuard } from "./guards/auth.guard";
+import { AuthenticatedGuard } from "./guards/authenticated.guard";
+
+const routes: Routes = [
+	{ path: "", pathMatch: "full", component: HomeComponent },
+	{
+		path: "auth",
+		component: AuthComponent,
+		canActivate: [AuthGuard],
+		children: [
+			{
+				path: "login",
+				component: LoginComponent,
+			},
+			{
+				path: "register",
+				component: RegisterComponent,
+			},
+		],
+	},
+	{
+		path: "profile",
+		component: ProfileComponent,
+		canActivate: [AuthenticatedGuard]
+	},
+	{
+		path: "channel/:id",
+		component: ChannelComponent,
+		canActivate: [AuthenticatedGuard, ChannelGuard],
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
