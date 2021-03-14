1cd59afd3d1bcec70a8c6747938cca6bca4b4a3d
David Snyder <davidgraygs4@gmail.com>
2021-03-14 16:40:23 -0400

    update the on sign up function

--- functions/src/onSignUp.ts
+++ functions/src/onSignUp.ts
@@ -1,21 +1,19 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import fetch from "node-fetch"
+import fetch from "node-fetch";
 
 export const createUserDoc = functions.auth.user().onCreate(async user => {
-	return await admin
-		.firestore()
-		.collection("users")
-		.doc(user.uid)
-		.set(
-			{
-				username: user.displayName,
-				photoURL: user.photoURL ?? "/avatar.png",
-				email: user.email,
-				displayName: user.displayName,
-				status: "",
-				channels: [],
-			},
-			{ merge: true }
-		);
+	const userDoc: any = {
+		photoURL: user.photoURL ?? "/avatar.png",
+		email: user.email,
+		status: "",
+		channels: [],
+	};
+
+	if (user.displayName) {
+		userDoc.username = user.displayName;
+		userDoc.displayName = user.displayName;
+	}
+
+	return await admin.firestore().collection("users").doc(user.uid).set(userDoc, { merge: true });
 });
