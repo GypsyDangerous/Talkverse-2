9b7a4ad2555843c320152b038053c5ec4854d859
David Snyder <davidgraygs4@gmail.com>
2021-03-17 16:01:00 -0400

    fix bugs

--- functions/src/conversations.ts
+++ functions/src/conversations.ts
@@ -2,7 +2,7 @@ import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
 export const aggregateMembers = functions.firestore
-	.document("conversations/${channelId}/members/{userId}")
+	.document("conversations/{channelId}/members/{userId}")
 	.onWrite(async (snapshot, context) => {
 		const { channelId, userId } = context.params;
 
